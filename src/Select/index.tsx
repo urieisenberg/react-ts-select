1e60c8587029ccb050f57a7bc32f551d2fb78fe4
CpCompany1017 <seretontour@gmail.com>
2022-11-02 01:35:56 +0200

    first commit

---
+++ src/Select/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {
+  SelectProps,
+  SelectOption,
+  MultipleSelectProps,
+  SingleSelectProps,
+} from "./types";
+
+
+export const Select = () => {
+  return <div>Select</div>;
+};
